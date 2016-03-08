16e7359856dac1ad9860581dd6308181c10afac3
Jarvis <JHester14@10.0.0.9>
2016-03-07 23:10:41 -0500

    new commit

--- src/com/java24hours/Saluton.java
+++ src/com/java24hours/Saluton.java
@@ -2,6 +2,7 @@ package com.java24hours;
 
 class Saluton {
     public static void main(String[] arguments) {
+        //program displayys the value stored in greeting
         String greeting = "Saluton mondo!!!";
         System.out.println(greeting);
     }
